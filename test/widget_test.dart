42f68d997765bd0e8b78034de5a1d2a45a792b5f
zoytsa <a.baturskiy@gmail.com>
2021-09-20 16:32:31 +0600

    next

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:lesson2/main.dart';
+import 'package:flutter_fight_club/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'dart:ui';
 
 void main() {
   group('l02h01', () {
-    testWidgets('Scaffold have right background color',
-        (WidgetTester tester) async {
+    testWidgets('Scaffold have right background color', (WidgetTester tester) async {
       await tester.pumpWidget(MyApp());
       expect(
         (tester.firstWidget(find.byType(Scaffold)) as Scaffold).backgroundColor,
@@ -16,27 +14,24 @@ void main() {
   });
 
   group("l02h02", () {
-    testWidgets(
-        'Buttons with BodyParts have right background in unselected state',
+    testWidgets('Buttons with BodyParts have right background in unselected state',
         (WidgetTester tester) async {
       const Color correctBackgroundColor = Colors.black38;
       const Color correctBackgroundColor2 = Color(0x60000000);
       await tester.pumpWidget(MyApp());
 
-      final coloredBoxInButtonFinder = (text) =>
-          findTypeByTextOnlyInParentType(ColoredBox, text, BodyPartButton);
+      final coloredBoxInButtonFinder =
+          (text) => findTypeByTextOnlyInParentType(ColoredBox, text, BodyPartButton);
 
       final List<ColoredBox> widgetsWithButtons = [
         ...tester.widgetList(coloredBoxInButtonFinder("HEAD")),
         ...tester.widgetList(coloredBoxInButtonFinder("TORSO")),
         ...tester.widgetList(coloredBoxInButtonFinder("LEGS")),
       ].cast<ColoredBox>().toList();
-      final Set<Color> allColors =
-          widgetsWithButtons.map((e) => e.color).toSet();
+      final Set<Color> allColors = widgetsWithButtons.map((e) => e.color).toSet();
       expect(allColors, isNotEmpty);
       expect(allColors.length, 1);
-      expect(allColors.single,
-          isOneOrAnother(correctBackgroundColor, correctBackgroundColor2));
+      expect(allColors.single, isOneOrAnother(correctBackgroundColor, correctBackgroundColor2));
     });
   });
 
@@ -45,37 +40,29 @@ void main() {
         'Tap on GO button clears selected attackingBodyPart and defendingBodyPart only when'
         ' selected both of them', (WidgetTester tester) async {
       await tester.pumpWidget(MyApp());
-      final GestureDetector goButton =
-          tester.widget(find.widgetWithText(GestureDetector, "GO"));
+      final GestureDetector goButton = tester.widget(find.widgetWithText(GestureDetector, "GO"));
       expect(goButton, isNotNull);
-      final List<GestureDetector> widgetsWithBodyParts = tester
-          .widgetList<GestureDetector>(
-              find.widgetWithText(GestureDetector, "HEAD"))
-          .toList();
+      final List<GestureDetector> widgetsWithBodyParts =
+          tester.widgetList<GestureDetector>(find.widgetWithText(GestureDetector, "HEAD")).toList();
       expect(widgetsWithBodyParts.length, 2);
 
       const Color selectedButtonColor = Color.fromRGBO(28, 121, 206, 1);
 
-      final coloredBoxInGestureDetectorFinder = (text) =>
-          findTypeByTextOnlyInParentType(ColoredBox, text, GestureDetector);
+      final coloredBoxInGestureDetectorFinder =
+          (text) => findTypeByTextOnlyInParentType(ColoredBox, text, GestureDetector);
 
-      final Color unselectedButtonColor = tester
-          .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first)
-          .color;
+      final Color unselectedButtonColor =
+          tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first).color;
 
       await tester.tap(find.widgetWithText(GestureDetector, "HEAD").first);
       await tester.pump();
 
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first).color,
         selectedButtonColor,
       );
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last).color,
         unselectedButtonColor,
       );
 
@@ -83,15 +70,11 @@ void main() {
       await tester.pump();
 
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first).color,
         selectedButtonColor,
       );
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last).color,
         unselectedButtonColor,
       );
 
@@ -99,15 +82,11 @@ void main() {
       await tester.pump();
 
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first).color,
         selectedButtonColor,
       );
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last).color,
         selectedButtonColor,
       );
 
@@ -115,15 +94,11 @@ void main() {
       await tester.pump();
 
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").first).color,
         unselectedButtonColor,
       );
       expect(
-        tester
-            .widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last)
-            .color,
+        tester.widget<ColoredBox>(coloredBoxInGestureDetectorFinder("HEAD").last).color,
         unselectedButtonColor,
       );
     });
@@ -135,16 +110,14 @@ void main() {
         ' is not selected', (WidgetTester tester) async {
       await tester.pumpWidget(MyApp());
 
-      final GestureDetector goButton =
-          tester.widget(find.widgetWithText(GestureDetector, "GO"));
+      final GestureDetector goButton = tester.widget(find.widgetWithText(GestureDetector, "GO"));
       expect(goButton, isNotNull);
 
       final gestureDetectorFinder =
           (text) => findTypeByTextOnlyInParentType(GestureDetector, text, Row);
 
-      final List<GestureDetector> widgetsWithBodyParts = tester
-          .widgetList<GestureDetector>(gestureDetectorFinder("HEAD"))
-          .toList();
+      final List<GestureDetector> widgetsWithBodyParts =
+          tester.widgetList<GestureDetector>(gestureDetectorFinder("HEAD")).toList();
       expect(widgetsWithBodyParts.length, 2);
 
       const Color unselectedButtonColor = Colors.black38;
@@ -152,8 +125,7 @@ void main() {
       const Color selectedGoButtonColor = Colors.black87;
       const Color selectedGoButtonColor2 = Color(0xDE000000);
 
-      final coloredBoxFinder =
-          (text) => findTypeByTextOnlyInParentType(ColoredBox, text, Row);
+      final coloredBoxFinder = (text) => findTypeByTextOnlyInParentType(ColoredBox, text, Row);
 
       expect(
         tester.widget<ColoredBox>(coloredBoxFinder("GO")).color,
@@ -179,19 +151,16 @@ void main() {
   });
 
   group('l01h05', () {
-    testWidgets('There are 5 ones below You and Enemy',
-        (WidgetTester tester) async {
+    testWidgets('There are 5 ones below You and Enemy', (WidgetTester tester) async {
       await tester.pumpWidget(MyApp());
 
-      final List<Text> widgetsWithLegs =
-          tester.widgetList<Text>(find.text("1")).toList();
+      final List<Text> widgetsWithLegs = tester.widgetList<Text>(find.text("1")).toList();
       expect(widgetsWithLegs.length, 10);
     });
   });
 }
 
-Matcher isOneOrAnother(dynamic one, dynamic another) =>
-    OneOrAnotherMatcher(one, another);
+Matcher isOneOrAnother(dynamic one, dynamic another) => OneOrAnotherMatcher(one, another);
 
 Finder findTypeByTextOnlyInParentType(
   final Type type,
@@ -212,11 +181,10 @@ class OneOrAnotherMatcher extends Matcher {
 
   @override
   Description describe(Description description) {
-    return description.add(
-        'either ${_one.runtimeType}:<$_one> or ${_another.runtimeType}:<$_another>');
+    return description
+        .add('either ${_one.runtimeType}:<$_one> or ${_another.runtimeType}:<$_another>');
   }
 
   @override
-  bool matches(Object? item, Map matchState) =>
-      item == _one || item == _another;
+  bool matches(Object? item, Map matchState) => item == _one || item == _another;
 }
