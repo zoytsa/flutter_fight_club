3566532a7f9eac2ec04ebdd7f8b925ba1b55584e
zoytsa <a.baturskiy@gmail.com>
2021-10-01 09:52:11 +0600

    next

---
+++ lib/widgets/action_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fight_club/fight_club_colors.dart';
+
+class GoButton extends StatelessWidget {
+  final bool readyToStart;
+  final String title;
+  final VoidCallback onGoButtonClicked;
+  GoButton(
+      {Key? key,
+      required this.readyToStart,
+      required this.onGoButtonClicked,
+      required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onGoButtonClicked(),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16.0),
+        height: 40,
+        alignment: Alignment.center,
+        color: readyToStart
+            ? FightClubColors.blackButton
+            : FightClubColors.greyButton,
+        child: Text(
+          title.toUpperCase(),
+          style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 16,
+              color: FightClubColors.whiteText),
+        ),
+      ),
+    );
+
+    return GestureDetector(
+        onTap: () => onGoButtonClicked(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: SizedBox(
+              height: 40,
+              // width: 40,
+              child: ColoredBox(
+                color: readyToStart
+                    ? FightClubColors.blackButton
+                    : FightClubColors.greyButton,
+                child: Center(
+                  child: Text(
+                    title.toUpperCase(),
+                    style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 16,
+                        color: FightClubColors.whiteText),
+                  ),
+                ),
+              )),
+        ));
+  }
+}
